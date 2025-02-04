faf1c788f3766e43467d4d66229fed0bbbe04638
AdrianSA <50012235+cloner69@users.noreply.github.com>
2025-02-04 10:47:41 +0700

    resizable header

---
+++ ResizableHeader/ResizableHeader/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  ResizableHeader
+//
+//  Created by Adrian Suryo Abiyoga on 22/01/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Home()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ResizableHeader/ResizableHeader/ResizableHeaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  ResizableHeaderApp.swift
+//  ResizableHeader
+//
+//  Created by Adrian Suryo Abiyoga on 22/01/25.
+//
+
+import SwiftUI
+
+@main
+struct ResizableHeaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
